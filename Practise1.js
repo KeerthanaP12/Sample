e250a6c8914b46ace00adc2cbc87df6f581e48bd
Keerthana <keerthana12041999@gmail.com>
2023-06-14 14:34:21 +0530

    added practise1 file for the first commit

---
+++ Practise1.js
@@ -0,0 +1,19 @@
+const readline = require("readline");
+
+const inp = readline.createInterface({
+    input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+    userInput.push(data);
+});
+
+inp.on("close", () => {
+    
+    var str = userInput[0];
+    
+    console.log(str.split('').join(','));   
+
+});
